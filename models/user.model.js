7c784e11bbdc7a44dc90a0341386896538cfc6a1
Omar630603 <omar.yem1111@gmail.com>
2023-02-09 13:35:58 +0700

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -10,11 +10,21 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Please enter your name"],
     },
+    username: {
+      type: String,
+      required: [true, "Please enter your username"],
+      unique: true,
+      lowercase: true,
+    },
     email: {
       type: String,
       required: [true, "Please enter your email"],
       unique: true,
       lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email",
+      ],
     },
     password: {
       type: String,
@@ -33,11 +43,27 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+// auto generate unique username from name
+userSchema.pre("save", function (next) {
+  this.username = this.name.toLowerCase().split(" ").join("");
+  next();
+});
+
 userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+// sign JWT and return
+userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {};
 
 // export model
 module.exports = mongoose.model("User", userSchema);
